45b892b748556c848155a96d6c5a4977312608cc
anilkothari1370 <anil.kothari@cognizant.com>
2025-09-19 11:29:14 -0400

    Add Django/pytest unit test skeleton for views

---
+++ test_views.py
@@ -0,0 +1,23 @@
+import pytest
+from django.urls import reverse
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ReservationViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+        self.client.login(username="testuser", password="testpass")
+
+    def test_home_page(self):
+        url = reverse("home")  # Replace "home" with your actual view name
+        response = self.client.get(url)
+        assert response.status_code == 200
+
+    # Example view test - adjust to your views/urls
+    # def test_reservation_list_view(self):
+    #     url = reverse("reservation-list")
+    #     response = self.client.get(url)
+    #     assert response.status_code == 200
